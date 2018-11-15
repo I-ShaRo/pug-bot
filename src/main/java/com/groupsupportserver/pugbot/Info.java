6ea274a5053ba014ddf66c2997e0717234cc0425
ShaRo <gerka668@gmail.com>
2018-11-15 14:45:49 +0100

    Env updated

--- src/main/java/com/groupsupportserver/pugbot/Info.java
+++ src/main/java/com/groupsupportserver/pugbot/Info.java
@@ -1,7 +1,9 @@
 package com.groupsupportserver.pugbot;
 
+import jdk.internal.util.EnvUtils;
+
 public class Info {
     public static String PREFIX = "&";
-    public static String TOKEN = process.env.token;
+    public static String TOKEN = EnvUtils.getEnvVar("token");
 }
 
